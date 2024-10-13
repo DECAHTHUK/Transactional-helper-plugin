c3d2c9e83285dc0f09a0ebf55901838f237fac46
DECAHTHUK <alexei.talanov003@mail.ru>
2024-10-13 14:06:35 +0300

    Building usage tree

--- src/main/java/ru/decahthuk/transactionhelperplugin/TransactionAnnotationHereInspection.java
+++ src/main/java/ru/decahthuk/transactionhelperplugin/TransactionAnnotationHereInspection.java
@@ -4,9 +4,24 @@ import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.JavaElementVisitor;
 import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
+import com.intellij.usageView.UsageInfo;
+import com.intellij.usages.Usage;
+import com.intellij.usages.UsageViewManager;
+import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TransactionAnnotationHereInspection extends AbstractBaseJavaLocalInspectionTool {
 
@@ -22,19 +37,40 @@ public class TransactionAnnotationHereInspection extends AbstractBaseJavaLocalIn
                     if (annotationName.equals("org.springframework.transaction.annotation.Transactional")) {
                         holder.registerProblem(annotation,
                                 InspectionBundle.message("inspection.transaction.annotation.here.descriptor"));
+
+                        List<PsiElement> psiElements = new ArrayList<>();
+
+                        ReferencesSearch.search(method).forEach(reference -> {
+                            PsiElement psiElement = reference.getElement();
+                            PsiMethod containingMethod = PsiTreeUtil.getParentOfType(psiElement, PsiMethod.class);
+                            PsiElement containingClass = PsiUtil.getTopLevelClass(containingMethod);
+                            psiElements.add(psiElement);
+                        });
+                        holder.registerProblem(method, psiElements.toString());
+
+                        buildUsageTree(method, 0);
                     }
                 }
             }
+        };
+    }
 
-            @Override
-            public void visitAnnotation(@NotNull PsiAnnotation annotation) {
-                super.visitAnnotation(annotation);
-                String annotationName = annotation.getQualifiedName();
-                if (annotationName.equals("org.springframework.transaction.annotation.Transactional")) {
-                    holder.registerProblem(annotation,
-                            InspectionBundle.message("inspection.transaction.annotation.here.descriptor-annotated"));
+    public static void buildUsageTree(PsiMethod method, int level) {
+        String indent = "  ".repeat(level);
+        System.out.println(indent + "Method: " + method.getName() + " in Class: " + method.getContainingClass().getQualifiedName());
+
+        Query<PsiReference> query = ReferencesSearch.search(method);
+        Set<PsiMethod> visitedMethods = new HashSet<>();
+
+        query.forEach(reference -> {
+            PsiElement element = reference.getElement();
+            if (element != null) {
+                PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
+                if (containingMethod != null && !visitedMethods.contains(containingMethod)) {
+                    visitedMethods.add(containingMethod);
+                    buildUsageTree(containingMethod, level + 1);
                 }
             }
-        };
+        });
     }
 }
